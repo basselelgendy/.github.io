114a7b7d260c1967743cc656bda846fea93ca9cf
Basel <opsawbs0@gmail.com>
2026-05-04 04:25:41 +0300

    Add files via upload

---
+++ next.config.js
@@ -0,0 +1,37 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  // السطر ده مهم جداً لـ GitHub Pages عشان يخرج الموقع كملفات ثابتة
+  output: "export",
+
+  env: {
+    TEMPLATE_ID: process.env.TEMPLATE_ID,
+    SERVICE_ID: process.env.SERVICE_ID,
+    PUBLIC_KEY: process.env.PUBLIC_KEY,
+  },
+
+  images: {
+    // السطر ده ضروري عشان جيت هاب ملوش سيرفر يعمل Optimize للصور
+    unoptimized: true,
+
+    // باقي إعداداتك زي ما هي
+    formats: ["image/avif", "image/webp"],
+    deviceSizes: [640, 750, 828, 1080, 1200, 1920],
+    imageSizes: [16, 32, 48, 64, 96, 128, 256],
+  },
+
+  compiler: {
+    removeConsole: process.env.NODE_ENV === "production",
+  },
+
+  experimental: {
+    optimizePackageImports: [
+      "framer-motion",
+      "react-icons",
+      "three",
+      "@react-three/fiber",
+      "@react-three/drei",
+    ],
+  },
+};
+
+module.exports = nextConfig;
